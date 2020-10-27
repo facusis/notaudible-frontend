7a87296da70bf2f6d6e7824b5838aa6f8987562a
Facundo <facusis@gmail.com>
2020-10-27 20:11:42 +0100

    cambios agregados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Navbar from './components/navbar'
 
 function App() {
   return (
---
+++ src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <nav className= 'navbar'>
+
+            <div className= 'navbar-container'>
+                Home
+                Bibliotecas
+                Register
+                login
+
+            </div>
+        </nav>
+
+    )
+
+
+}
+
+
+export default NavBar
