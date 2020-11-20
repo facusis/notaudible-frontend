664595587a05280b3446b679697a198d17197156
gabviana <gabriela_viana13@hotmail.com>
2020-11-20 18:51:31 +0100

    commit crear componente biblioteca

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 import Register from './components/Register/Register';
+import Biblioteca from './components/Biblioteca/Biblioteca';
 import Home from './components/Home/Home';
 import {BrowserRouter,
 Link,
 Switch,
 Route
 } from 'react-router-dom';
-
+import {HOME, BIBLIOTECA, REGISTER, LOGIN} from './routes';
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
+          <Route path={HOME} exact component={Home} />
+          <Route path={LOGIN} component={Login} />
+          <Route path={REGISTER} component={Register} />
+          <Route path={BIBLIOTECA} component={Biblioteca} />
         </Switch>
       </BrowserRouter>
     </div>
