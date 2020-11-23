bbe6deae968b87066eda94b4b3a1ec7fcc152870
Facundo <facusis@gmail.com>
2020-11-23 20:39:31 +0100

    fetch crear libro

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -15,25 +15,33 @@ export default function Library() {
     })
   }
 
-  const token = localStorage.getItem("token");
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-  console.log(data);
+    //localhost:3000/data/User
+    fetch('http://localhost:3001/data/book',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept' : 'application/json',
+        'Authorization': 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYTQzZTcwY2U3Zjk3MmIwZDVjMjM0NyIsImlhdCI6MTYwNTYzOTIxMX0.auP3ifPMSB0mJLpFUEFVzUNSxrInEVjHVeU22TEbWfg'
+      },
+      body: JSON.stringify({title: data.title, author: data.author, category: data.category})
+    }).then(res => res.json()).then((result) => {console.log(result)});
+  }
 
 
     return (
       <div>
-          <form className={'form-library'}>
             <label>Titulo del libro</label><br/>
           <input className={'input-library'} type="text" name="title" placeholder="Titulo del libro" required onChange={handleImputChange}/> <br/>
-          <label>Titulo del libro</label><br/>
+          <label>Categoria</label><br/>
 
           <input className={'input-library'} type="text" name="category" placeholder="Categoria del libro" required onChange={handleImputChange}/> <br/>
           <label>Autor</label><br/>
           <input className={'input-library'} type="text" name="author" placeholder="Autor del libro" required onChange={handleImputChange}/><br/>
-          <button type="submit">Crear libro</button>
-          </form>
+          <button onClick={handleSubmit}>Crear libro</button>
 
       </div>
-   
     )
   }
