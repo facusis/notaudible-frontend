6f6fa16cf0d48215441dee669e80833d2f12f8da
crispantufla <crispantufla@gmail.com>
2020-11-25 14:59:29 +0100

    funcion IsLogin

--- src/App.js
+++ src/App.js
@@ -5,13 +5,23 @@ import ForgetPass from './components/Login/ForgetPass'
 import Navbar from './components/Navbar/Navbar';
 import Register from './components/Register/Register';
 import Biblioteca from './components/Biblioteca/Biblioteca';
+import UserPanel from './components/UserPanel/UserPanel';
 import Home from './components/Home/Home';
 import {BrowserRouter,
 Link,
 Switch,
-Route
+Route,
+Redirect
 } from 'react-router-dom';
-import {HOME, BIBLIOTECA, REGISTER, LOGIN} from './routes';
+import {HOME, BIBLIOTECA, REGISTER, LOGIN, PERFIL} from './routes';
+import {isLogin} from './components/Utils/isLogin';
+
+const PrivateRoute = ({ component: Component, path }) => (
+  <Route path={path} render={() => {
+    return isLogin() ? <Component /> : <Redirect to="/" />;
+    }}
+  />
+)
 
 function App() {
   return (
@@ -23,6 +33,7 @@ function App() {
           <Route path={LOGIN} component={Login} />
           <Route path={REGISTER} component={Register} />
           <Route path={BIBLIOTECA} component={Biblioteca} />
+          <PrivateRoute path={PERFIL} component={UserPanel} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const HOME = "/";
 const BIBLIOTECA = "/biblioteca";
 const LOGIN = '/login';
 const REGISTER = '/register';
+const PERFIL = '/perfil'
 
 export {
     HOME,
     BIBLIOTECA,
     LOGIN,
-    REGISTER
+    REGISTER,
+    PERFIL
 } 
