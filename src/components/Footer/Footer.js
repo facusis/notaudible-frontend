bcda74606b031feb0bdab662d27a34eeff579fd1
Facundo <facusis@gmail.com>
2020-12-03 20:59:53 +0100

    facu 3 sprint

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,17 +7,16 @@ const footer = () => {
        <>
         <div className={"main-footer"}>
             <div className="container">
-                <div className="rows">
-                {/* {Columna 1} */}
+                                
                     <div className="col">
                         <h2>BARCELONA</h2>
                         <ul className="list-unstyled">
-                            <li>Carrer Padilla 78</li>
+                            <li>Carrer Padilla 78.</li>
                             <li>TEL: 123 456 789</li>
-                            <li>08011 Barcelona</li>
+                            <li>08011 Barcelona.</li>
                         </ul>
                     </div>
-                    {/* {Columna 2} */}
+                    
                     <div className="col1">
                         <h4> ICONOS REDES </h4>
                         <ul className="list-unstyled1">
@@ -26,15 +25,13 @@ const footer = () => {
                             <li> Twitter </li>
                         </ul>
                     </div>
-                </div>
             
-            <div className="row"> 
+        </div>
+        <div className="row"> 
                 <p className="col-small"> Copyright © 2020 NOTAUDIBLE INC. <a className= "letras" href="https://nuclio.school/politica-de-privacidad/"> Política de privacidad y cookies - </a> <a className= "letras" href="https://nuclio.school/condiciones-legales/"> Aviso legal </a> </p>
-                
                 <br></br>
                 <p className="frase"> MADE WITH LOVE BY US </p>
             </div> 
-        </div>
     </div>
          
        </>
--- src/components/UploadBook/UploadBook.js
+++ src/components/UploadBook/UploadBook.js
@@ -4,7 +4,7 @@ import {fetchResource} from "../../api";
 import './UploadBook.css';
 
 const UploadBook = () => {
-  
+
   const [data, setData] = useState({});
   const [category, setCategory] = useState()
 
@@ -15,35 +15,77 @@ const UploadBook = () => {
     })
   }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-        fetchResource('upload','','POST', {
-            title: data.title, 
-            author: data.author, 
-            category: category,
-            sinopsis: data.sinopsis,
-        }).then((result) => {console.log(result)});
-    }
-
-    return (
-            
-          <div className={"uploadForm"}>
-          <label className={"labelUpload tituloUpload"}>SUBE UN LIBRO</label><br/>
-          <label className={"labelUpload"}>Titulo</label><br/>
-          <input className={'input-library'} type="text" name="title"required onChange={handleImputChange}/> <br/>
-          <label className={"labelUpload"}>Categoria</label><br/>
-          <SelectRequest setCategory={setCategory} request="category"></SelectRequest><br/>
-          <label className={"labelUpload"}>Autor</label><br/>
-          <input className={'input-library'} type="text" name="author" required onChange={handleImputChange}/><br/>
-          <label className={"labelUpload"}>Sinopsis</label><br/>
-          <input className={'sinopsis'} type="text" name="sinopsis" required onChange={handleImputChange}/><br/>
-          <label className={"labelUpload"}>Archivo</label><br/>
-          <input className={'input-library'} type="file" name="archivo"required onChange={handleImputChange}/><br/>
-          <button className={"submitButton"}onClick={handleSubmit}>Crear libro</button>
-      </div>
+    fetchResource('upload','','POST', {
+      title: data.title, 
+      author: data.author, 
+      category: category,
       
-    )
+    }).then((result) => {console.log(result)});
   }
+
+  return (
+      
+    <div className={"uploadForm"}>
+      <label className={"labelUpload tituloUpload"}>
+        SUBE UN LIBRO
+      </label><br/>
+      <label className={"labelUpload"}>
+        Titulo
+      </label><br/>
+      <input 
+        className={'input-library'} 
+        type="text" 
+        name="title"
+        required 
+        onChange={handleImputChange}
+      /><br/>
+      <label className={"labelUpload"}>
+        Categoria
+      </label><br/>
+      <SelectRequest 
+        setCategory={setCategory} 
+        request="category">
+      </SelectRequest><br/>
+      <label className={"labelUpload"}>
+        Autor
+      </label><br/>
+      <input 
+        className={'input-library'} 
+        type="text" 
+        name="author" 
+        required 
+        onChange={handleImputChange}
+      /><br/>
+      <label className={"labelUpload"}>
+        Sinopsis
+      </label><br/>
+      <textarea 
+        className={'sinopsis'} 
+        type="text" 
+        name="sinopsis" 
+        required onChange={handleImputChange}
+      /><br/>
+      <label className={"labelUpload"}>
+        Archivo
+      </label><br/>
+      <input 
+        className={'input-library'} 
+        type="file" 
+        name="archivo"
+        required 
+        onChange={handleImputChange}
+        /><br/>
+      <button 
+        className={"submitButton"}
+        onClick={handleSubmit}>
+        Crear libro
+      </button>
+    </div>
+
+  )
+}
 
 export default UploadBook;
