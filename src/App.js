1a116b4eb151e8e17a6c2f30dc80e651061f002a
crispantufla <crispantufla@gmail.com>
2020-12-07 14:08:09 -0800

    CategorysBox y more fixes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Login from './components/Login/Login';
 import Navbar from './components/Navbar/Navbar';
 import Register from './components/Register/Register';
 import Biblioteca from './components/Biblioteca/Biblioteca';
 import UserPanel from './components/UserPanel/UserPanel';
 import Home from './components/Home/Home';
-import UploadBook from './components/UploadBook/UploadBook';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Forgetpass from './components/Login/ForgetPass';
 import PageUploadBook from './components/UploadBook/PageUploadBook';
 import Footer from './components/Footer/Footer';
+import LogedContext from './LogedContext';
 import {BrowserRouter,
 Switch,
 Route
 } from 'react-router-dom';
 import {HOME, BIBLIOTECA, REGISTER, LOGIN, PERFIL, UPLOADBOOK, FORGETPASS} from './routes';
-
+import { isLogin } from './utils/loginUtils';
 
 function App() {
+
+  const [loged, setLoged] = useState(isLogin);
+  
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path={HOME} exact component={Home} />
-          <Route path={LOGIN} exact component={Login} />
-          <Route path={REGISTER} component={Register} />
-          <Route path={BIBLIOTECA} component={Biblioteca} />
-          <PrivateRoute path={PERFIL} component={UserPanel} />
-          <PrivateRoute path={UPLOADBOOK} component={PageUploadBook} />
-          <Route path={FORGETPASS} exact component={Forgetpass} />
-        </Switch>
+        <LogedContext.Provider value={{setLoged, loged}}>
+          <Navbar />
+          <Switch>
+            <Route path={HOME} exact component={Home} />
+            <Route path={LOGIN} exact component={Login} />
+            <Route path={REGISTER} component={Register} />
+            <Route path={BIBLIOTECA} component={Biblioteca} />
+            <PrivateRoute path={PERFIL} component={UserPanel} />
+            <PrivateRoute path={UPLOADBOOK} component={PageUploadBook} />
+            <Route path={FORGETPASS} exact component={Forgetpass} />
+          </Switch>
+        </LogedContext.Provider>
       </BrowserRouter>
       <div className="content-wrap">  
       </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import {isLogin, logOut} from '../../utils/loginUtils';
+import LogedContext from '../../LogedContext';
 
 const Navbar = () => {
+
+    const loged = useContext(LogedContext);
+
+    useEffect(() => {
+        console.log(loged.loged);
+    }, [loged.loged]);
+
+    const trueLogOut = () =>{
+        logOut()
+        loged.setLoged(false);
+    }
+
     return (
         <nav className='navbar'>
             <div className='navbar-container'>
                 
+
                 {isLogin() ? 
                     <div className='nav-links'> 
                         <Link to="/" >Home</Link>
                         <Link to="/biblioteca" >Bibliotecas</Link>
                         <Link to="/upload"> Subir Audiolibro </Link> 
                         <Link to="/perfil"> Perfil </Link> 
-                        <Link className="nav-cerrarsesion" to="/perfil" onClick={logOut}> Cerrar Sesión </Link> 
+                        <Link className="nav-cerrarsesion" to="/perfil" onClick={trueLogOut}> Cerrar Sesión </Link> 
                     </div> : 
                     <div className='nav-links'> 
                         <Link to="/" >Home</Link>
