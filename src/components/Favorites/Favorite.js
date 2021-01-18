c87810d782050a95023ca9ddf45f16d2097ca8f5
facusis <facusis@gmail.com>
2021-01-18 22:52:53 +0100

    cambios facu sprint 5 18-01

--- src/components/Favorites/Favorite.js
+++ src/components/Favorites/Favorite.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faBook} from '@fortawesome/free-solid-svg-icons';
+import { fetchResource } from '../../api';
 
 const Favorite = () => {
 
-    
+  const addFavorite = () => {
+      
+  }
 
 
     return ( 
         <div>
-            <button className='botonFav'> fav 
-                <FontAwesomeIcon icon={faBook} />
+            <button className='botonFav' onClick= {addFavorite}>
+                <FontAwesomeIcon icon={faBook} size="2x"/>
                 
             </button>
                 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import {faFacebook, faInstagram, faSpotify} from '@fortawesome/free-brands-svg-icons';
 
 const footer = () => {
     return (
@@ -17,18 +20,19 @@ const footer = () => {
                     
                     <div className="col1">
                         <h2> SOCIAL NETWORKS </h2>
-                            <ul className="list-unstyled1">
-                                <li> Facebook </li>
-                                <li> Instagram </li>
-                                <li> Twitter </li>
-                            </ul>
+                            <div className="list-unstyled1">
+                                <a href="https://www.facebook.com/AudibleES/?brand_redir=90486735811">  <FontAwesomeIcon className= "icono" icon={faFacebook}  /> </a>
+                                <a  href="https://www.instagram.com/audible/?hl=es"><FontAwesomeIcon className= "icono" icon={faInstagram} /></a>
+                                <a href="https://open.spotify.com/artist/0pk95otj3wM2dlj76O988X"> <FontAwesomeIcon className= "icono" icon={faSpotify} /> </a>
+                            </div>
                     </div>
             
         </div>
             <div className="rowe"> 
                 <p className="col-small"> Copyright © 2020 NOTAUDIBLE INC. <a className= "letras" href="https://nuclio.school/politica-de-privacidad/"> Política de privacidad y cookies - </a> <a className= "letras" href="https://nuclio.school/condiciones-legales/"> Aviso legal </a> </p>
                 <br></br>
-                <p className="frase"> MADE WITH LOVE BY US </p>
+                <p className="frase"> 
+                MADE WITH <FontAwesomeIcon icon={faHeart} style={{ color: '#b48484' }}/> BY US </p>
             </div> 
         </div>
          
