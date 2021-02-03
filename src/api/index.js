3fcf6f1635af5214f657b6cb5bc515da19229ed4
gorkatg <gorkatg@gmail.com>
2021-02-03 21:41:49 +0100

    API fetch

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,5 @@
-const API_URL = window.location.hostname === 'notaudible.netlify.app' ? "https://notaudible.herokuapp.com" : "http://localhost:3001"
-
 
 export const fetchResource = (resource, id, method, data, options={}, headers={}) => {
-
 
     const API_URL = window.location.hostname === 'notaudible.netlify.app' ? "https://notaudible.herokuapp.com" : "http://localhost:3001"
 
