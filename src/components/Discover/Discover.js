a52c09590e0c4e7dc44a79957770c3738a8c576e
gorkatg <gorkatg@gmail.com>
2021-02-05 11:01:32 +0100

    Discover and Follow from Zhiting

---
+++ src/components/Discover/Discover.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./Discover.css";
+//import UserCard from "../UserProfile/UserCard";
+import {fetchResource} from '../../api';
+import Follow from '../Follow/Follow';
+
+const Discover = () => {
+
+  const [users, setUsers] = useState([
+    {
+      nickname: "",
+      email: "",
+    },
+  ]);
+
+
+  useEffect(() => {
+    fetchResource("user", undefined, 'GET', undefined,{},{})
+        .then(jsonres => setUsers(jsonres));
+
+  }, []);
+
+//   let usersToRender;
+//   if (users) {
+//       usersToRender = users.map(user => {
+//           return (
+//             <div key={user.id}>
+//                 <h4>{user.nickname}</h4>
+//                 <p>{user.email} </p>
+//             </div>
+//           )
+//       }); 
+//   } else {
+//       usersToRender = "Loading ...";
+//   }
+
+
+  return ( 
+    <div className="userProfiles">
+      <h2>Discover users</h2>
+      <div className="userProfiles__userCard">
+       {users && users.length > 0
+       ? users.map(user => {
+           return (
+             <div>
+              {user.nickname}
+              {user.email}
+              {user && <Follow user={user} /> }
+            </div>
+           )
+       })
+       : "Loading..."
+       }
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
---
+++ src/components/Follow/Follow.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState} from "react";
+import {fetchResource} from '../../api';
+
+const Follow = (props) => {
+
+    const [following, setFollowing] = useState(false);
+
+    const [followId, setFollowId] = useState('');
+
+// to fetch the data of the user that I want to follow
+    // const fetchUserData = () => {
+    //     fetch(`http://localhost:3001/user/${id}`)
+    //         .then(res => res.json())
+    //         .then(data => setUser(data))
+    // };
+
+    // useEffect(() => {
+    //     fetchUserData()
+    // }, []);
+
+    console.log(props.user._id); // from Discover component
+
+
+    const unFollow = () => {
+        fetchResource('data/follow', followId._id, 'DELETE', {})
+        setFollowing(false);
+        console.log('Unfollowed');
+    };
+
+    console.log(followId._id);
+
+    const handleFollow = () => {
+        fetch(`http://localhost:3001/user/follow`, {
+            method: "POST",
+            body:  JSON.stringify({
+                follower: localStorage.getItem('id'),  // logged user id
+                following: props.user._id
+            }),
+            headers: {
+                "Content-type": "application/json",
+                "Authorization": `bearer ${localStorage.getItem("token")}`,
+            },
+
+        })
+        .then((res) => res.json())
+        .then((res) => {
+            setFollowId(res);
+            setFollowing(true);
+            console.log('Follwed')
+    });
+};
+
+    const showFollowButton =  following ?
+    (<button onClick={unFollow}>Unfollow</button>)
+    : 
+    (<button onClick={handleFollow}>Follow</button>)
+
+    return (
+        <div>
+         {props.user._id === localStorage.getItem('id') ? null : showFollowButton }
+        </div>
+    );
+
+};
+
+export default Follow;
